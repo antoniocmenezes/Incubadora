61df87129f78cee943415b0debeabcf8b2a5ea6c
Antonio Lucas <antoniolucascabral0@gmail.com>
2025-11-01 18:36:07 -0300

    troca url

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,5 @@
 // ================== CONFIG ==================
-const API = 'incubadora-production.up.railway.app/api';
+const API = 'https://incubadora-production.up.railway.app/api';
 
 // ================== AUTH STORAGE ==================
 function token() { return localStorage.getItem('token'); }
